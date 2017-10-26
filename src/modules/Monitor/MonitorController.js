dc114324ebe5cd83f46f039e47fcfb0f261405cb
labroche <corentin.labroche@inist.fr>
2017-10-26 10:31:44 +0200

    say that an error is occured on the modules view

--- src/modules/Monitor/MonitorController.js
+++ src/modules/Monitor/MonitorController.js
@@ -1,4 +1,21 @@
-function MonitorController ($scope, $interval, WorkersService, ConfigService, $state, ModuleService) {
-  $scope.activeModule = ModuleService.changeActiveModule('Monitor');
+function MonitorController(
+  $scope,
+  $interval,
+  WorkersService,
+  ConfigService,
+  $state,
+  ModuleService
+) {
+  $scope.activeModule = ModuleService.changeActiveModule("Monitor");
+  $interval(_ => {
+    const logs = WorkersService.getLogs();
+    $scope.thereIsAnErrorInLog =
+      logs &&
+      logs.hasOwnProperty("error") &&
+      Array.isArray(logs.error) &&
+      logs.error.length > 0
+        ? true
+        : false;
+  }, ConfigService.refresh);
 }
 module.exports = MonitorController;
