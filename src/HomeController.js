3fc81a7894f057139508627d825ca0c221f8c251
labroche <corentin.labroche@inist.fr>
2017-11-22 13:52:40 +0100

    redis for linux

--- src/HomeController.js
+++ src/HomeController.js
@@ -1,38 +1,81 @@
 const request = require('request-promise');
-function HomeController ($scope, $interval, ModuleService, $state, ConfigService, NotificationService, WorkersService) {
-  const loadWorkers = $interval(_ => {
-    if (!ConfigService.get('workers')) {
-      request(ConfigService.get('serverUrl') + 'workers').then(function (workers) {
-        workers = JSON.parse(workers);
-        workers = workers.workers.map(function (worker) {
-          return { name: worker, disable: false };
-        });
-        ConfigService.save({ workers });
-        clearInterval(loadWorkers);
-      });
-    }
-  }, 1000);
+const cp = require("child_process");
+const pathfs = require('path')
+const Promise = require('bluebird')
+const redis = require("redis");
+Promise.promisifyAll(redis.RedisClient.prototype);
+Promise.promisifyAll(redis.Multi.prototype);
+const client = redis.createClient();
+let redisConnection = false
+client.on("error", _ => (redisConnection = false));
+client.on("ready", _ => (redisConnection = true));
+
+
+// HomeController.$inject = [ "$scope", "$interval", "ModuleService", "$state", "ConfigService", "NotificationService", "WorkersService" ];
+function HomeController ( $scope, $interval, ModuleService, $state, ConfigService, NotificationService, WorkersService, ModalService) {
+  HomeController.$inject.map((dependency, index)=> this[dependency]= arguments[index])
+  this.loadWorkers()
+  this.checkUpdates()
+  this.checkConnection()
+  this.loadHost()
+  this.launchRedis()
+  if (!ConfigService.get('debug')) ConfigService.save({debug: false})
+}
+HomeController.prototype.launchRedis = function() {
+  if (!redisConnection) {
+    const pathToRedisServer = pathfs.resolve(__dirname, '..', 'lib', 'redis-linux-64', 'src', 'redis-server')
+    const spawnRedis = cp.spawn(pathToRedisServer)
+    setTimeout(function() {
+      spawnRedis.kill('SIGTERM')
+    }, 3000);
+  }
+};
+
+HomeController.prototype.loadHost = function () {
+  const $scope = this.$scope
+  const ConfigService = this.ConfigService
+  const WorkersService = this.WorkersService
+  const host = ConfigService.get('host');
+  if (!host) ConfigService.save({ host: 'localhost', serverUrl: 'http://localhost:3264/' });
+  WorkersService.changeHost(host);
+  $scope.Model = { host };
+  
+}
+
+HomeController.prototype.checkConnection = function () {
+  const $scope = this.$scope
+  const $interval = this.$interval
+  const ConfigService = this.ConfigService
   $interval(_ => {
-    const serverConnection = $scope.serverConnection
-    request({ url: ConfigService.get('serverUrl') + 'ping', timeout: 2000 })
+    const serverConnection = $scope.serverConnection;
+    request({ url: ConfigService.get("serverUrl") + "ping", timeout: 2000 })
       .then(data => {
-        if (data === 'pong') $scope.serverConnection = true;
+        if (data === "pong") $scope.serverConnection = true;
         else $scope.serverConnection = false;
       })
       .catch(_ => ($scope.serverConnection = false));
   }, 1000);
-  const host = ConfigService.get('host');
-  if (!ConfigService.get('debug')) {
-    ConfigService.save({debug: false})
-  }
-  if (!host) {
-    ConfigService.save({
-      host: 'localhost',
-      serverUrl: 'http://localhost:3264/'
-    });
-  }
-  $scope.Model = { host };
-  WorkersService.changeHost(host);
+}
+
+HomeController.prototype.loadWorkers = function(){
+  const ConfigService = this.ConfigService;
+  const $interval = this.$interval
+  $interval( function () {
+    if (!ConfigService.get("workers")) {
+      request(ConfigService.get("serverUrl") + "workers")
+        .then(function(workers) {
+          workers = JSON.parse(workers);
+          workers = workers.workers.map(worker => ({ name: worker, disable: false }));
+          ConfigService.save({ workers });
+          clearInterval(this);
+        }); 
+    }
+  }, ConfigService.refresh);
+}
+
+HomeController.prototype.checkUpdates = function () {
+  const $state = this.$state
+  const NotificationService = this.NotificationService
   if (!$state.alreadyCheckIfUpdateAvailable) {
     request({ 
       url: 'https://api.github.com/repos/istex/sisyphe-monitor/releases/latest', 
@@ -52,6 +95,5 @@ function HomeController ($scope, $interval, ModuleService, $state, ConfigService
     })
     $state.alreadyCheckIfUpdateAvailable = true
   }
-  // $state.go('Monitor');
 }
 module.exports = HomeController;
